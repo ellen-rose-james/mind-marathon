b3a4ad2d961e5f28ef8904c6000c1a76b16da5f1
ellen-rose-james <ellenrj03@gmail.com>
2025-05-14 14:34:20 +0530

    fix: correct answer count updates and displays correctly

--- assets/js/finish.js
+++ assets/js/finish.js
@@ -1,4 +1,3 @@
-
 // Retrieve the number of correct answers from localStorage
 const correctAnswers = localStorage.getItem("correctAnswers") || 0;
 document.getElementById("correctAnswers").textContent = correctAnswers;
