105120c2a403e53f41de1ccd4c62cdd96767c58a
ellen-rose-james <ellenrj03@gmail.com>
2025-05-14 14:26:32 +0530

    fix: separate assets from html code

---
+++ assets/js/finish.js
@@ -0,0 +1,4 @@
+
+// Retrieve the number of correct answers from localStorage
+const correctAnswers = localStorage.getItem("correctAnswers") || 0;
+document.getElementById("correctAnswers").textContent = correctAnswers;
