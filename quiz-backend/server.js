52f72bc216072c5ae886262ff74cfb56e749f06f
ellen-rose-james <ellenrj03@gmail.com>
2025-05-16 00:18:12 +0530

    feat: configure MongoDB connection using environment variable for improved security

--- quiz-backend/server.js
+++ quiz-backend/server.js
@@ -12,10 +12,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-const mongoURI =
-  "mongodb+srv://ellenrj03:drpzk261g4IQnROI@quizdb.v0vtrue.mongodb.net/";
+require('dotenv').config();
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
