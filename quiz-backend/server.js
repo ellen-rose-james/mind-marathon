ca6e66bc6015f9a7045cf300a722c0a3e0378c79
ellen-rose-james <ellenrj03@gmail.com>
2025-05-16 10:05:24 +0530

    Revert "feat: configure MongoDB connection using environment variable for improved security"
    This reverts commit 52f72bc216072c5ae886262ff74cfb56e749f06f.

--- quiz-backend/server.js
+++ quiz-backend/server.js
@@ -12,9 +12,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-require('dotenv').config();
+const mongoURI =
+  "mongodb+srv://ellenrj03:drpzk261g4IQnROI@quizdb.v0vtrue.mongodb.net/";
 mongoose
-.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
