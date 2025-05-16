311b29452a8eb2ec3fac9ef637678df93cb8bed8
ellen-rose-james <ellenrj03@gmail.com>
2025-05-16 10:04:50 +0530

    Revert "feat: add dotenv package for environment variable management"
    This reverts commit 92e3e1c47d6931518ebc25f164e91eaa689cebf7.

--- quiz-backend/server.js
+++ quiz-backend/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 // MongoDB connection
 require('dotenv').config();
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
