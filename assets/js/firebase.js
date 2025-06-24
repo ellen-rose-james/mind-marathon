462a2981d1b0bb9b375e41a6e0a1e8f9b15c1307
ellen-rose-james <ellenrj03@gmail.com>
2025-06-24 20:03:06 +0530

    feat: add Firebase configuration and initialization

---
+++ assets/js/firebase.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6JEG5j9tb6XbKhT0E5wxXYSphNvpRPJw",
+  authDomain: "mind-marathon-db.firebaseapp.com",
+  projectId: "mind-marathon-db",
+  storageBucket: "mind-marathon-db.firebasestorage.app",
+  messagingSenderId: "158031876314",
+  appId: "1:158031876314:web:7c212dd486416992b055e3"
+};
+
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.firestore();
